67283104e063e47b6eca65fd27ca08107d75ed2e
lizhou <lizhou1021@qq.com>
2024-05-20 22:41:43 +0800

    chore: 样式调整

--- src/app.tsx
+++ src/app.tsx
@@ -85,10 +85,47 @@ export function App() {
   }, []);
 
   return (
-    <div>
+    <div className="w-full h-full">
       {list.map((item, index) => (
         <div key={index}>{item.url}</div>
       ))}
+      {/* <ul role="list" className="divide-y divide-gray-100">
+        {list.map((item) => (
+          <li key={item.url} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+              <img
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                src={item.imageUrl}
+                alt=""
+              />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {item.name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {item.email}
+                </p>
+              </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">{item.role}</p>
+              {item.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen{" "}
+                  <time dateTime={item.lastSeenDateTime}>{item.lastSeen}</time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul> */}
       <button onClick={handleDownload}>下载</button>
     </div>
   );
