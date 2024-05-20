0b874e85649126eaff64825b5feb602c1cf02ba2
lizhou <lizhou1021@qq.com>
2024-05-20 22:11:27 +0800

    feat: 加入下载功能

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,64 @@ import preactLogo from "./assets/preact.svg";
 import viteLogo from "/vite.svg";
 import "./app.css";
 import { getActivaTab } from "./utils/message";
+import JSZip from "jszip";
+
+function getFileNameFromUrl(url) {
+  // 使用正则表达式从 URL 中提取文件名
+  var matches = url.match(/\/([^\/?#]+)$/);
+  if (matches && matches.length > 1) {
+    return matches[1]; // 返回匹配到的文件名
+  } else {
+    return null; // 如果未匹配到文件名，返回 null
+  }
+}
 
 export function App() {
   const [list, setList] = useState<chrome.webRequest.WebRequestBodyDetails[]>(
     []
   );
+
+  const handleDownload = () => {
+    // 创建一个新的 JSZip 实例
+    const zip = new JSZip();
+
+    const resources = list.map((item) => ({
+      url: item.url,
+      filename: getFileNameFromUrl(item.url),
+    }));
+    let len = resources.length;
+    let i = 0;
+    console.log(`共${len}个资源`);
+    // 下载并压缩资源
+    Promise.all(
+      resources.map(function (resource) {
+        return fetch(resource.url)
+          .then(function (response) {
+            if (!response.ok) {
+              throw new Error("下载失败: " + response.status);
+            }
+            return response.blob();
+          })
+          .then(function (blob) {
+            // 将下载的资源添加到压缩包中
+            console.log(`第${i++}/${len}个资源`);
+            zip.file(resource.filename, blob);
+          });
+      })
+    )
+      .then(function () {
+        // 生成压缩包
+        return zip.generateAsync({ type: "blob" });
+      })
+      .then(function (content) {
+        // 下载压缩包
+        var url = URL.createObjectURL(content);
+        var link = document.createElement("a");
+        link.href = url;
+        link.download = "resources.zip";
+        link.click();
+      });
+  };
 
   useEffect(() => {
     const callback = function (message, sender, sendResponse) {
@@ -36,6 +89,7 @@ export function App() {
       {list.map((item, index) => (
         <div key={index}>{item.url}</div>
       ))}
+      <button onClick={handleDownload}>下载</button>
     </div>
   );
 }
